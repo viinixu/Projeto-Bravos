80e0381fb2cb50f08446d7fd3db8b810fe63de99
Brenão <brdriguesdesouza@gmail.com>
2026-07-26 17:23:51 -0300

    js consertado.

--- TrabalhoFinal/script.js
+++ TrabalhoFinal/script.js
@@ -2,39 +2,27 @@ const btnReserva = document.getElementById("reserva");
 const btnMenu = document.getElementById("visualizarMenu");
 
 btnReserva.addEventListener("click", () => {
-
     localStorage.setItem("ultimaAcao","Reserva");
-
     alert("Sua reserva foi iniciada!");
-
 });
-
 btnMenu.addEventListener("click", () => {
-
     localStorage.setItem("ultimaAcao","Menu");
-
     const menu = document.getElementById("Menu");
-
     if(menu){
-
         menu.scrollIntoView({
             behavior:"smooth"
         });
-
     }else{
-
         alert("Menu em desenvolvimento.");
-
     }
-    // Lógica do Menu Responsivo
+});
+
+// Lógica do Menu Responsivo
 const menuToggle = document.getElementById("menu-toggle");
 const navegacao = document.getElementById("navegacao");
 
 if (menuToggle && navegacao) {
     menuToggle.addEventListener("click", () => {
-        // A classe 'ativo' foi criada no CSS para mostrar a nav
         navegacao.classList.toggle("ativo");
     });
 }
-
-});
