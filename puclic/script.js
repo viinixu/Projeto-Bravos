2e3b012be2c3436a5eedf899ead45df0696105c2
Brenão <brdriguesdesouza@gmail.com>
2026-07-26 21:20:26 -0300

    Adicionando a página de reserva de mesas

--- puclic/script.js
+++ puclic/script.js
@@ -1,18 +1,19 @@
 const btnReserva = document.getElementById("reserva");
 const btnMenu = document.getElementById("visualizarMenu");
 
+
 btnReserva.addEventListener("click", () => {
-    localStorage.setItem("ultimaAcao","Reserva");
-    alert("Sua reserva foi iniciada!");
+    localStorage.setItem("ultimaAcao", "Reserva");
+    window.location.href = "reserva.html";
 });
 btnMenu.addEventListener("click", () => {
-    localStorage.setItem("ultimaAcao","Menu");
+    localStorage.setItem("ultimaAcao", "Menu");
     const menu = document.getElementById("Menu");
-    if(menu){
+    if (menu) {
         menu.scrollIntoView({
-            behavior:"smooth"
+            behavior: "smooth"
         });
-    }else{
+    } else {
         alert("Menu em desenvolvimento.");
     }
 });
@@ -24,5 +25,34 @@ const navegacao = document.getElementById("navegacao");
 if (menuToggle && navegacao) {
     menuToggle.addEventListener("click", () => {
         navegacao.classList.toggle("ativo");
+    });
+}
+
+// LÓGICA DA PÁGINA DE RESERVAS
+const formReserva = document.getElementById("formReserva");
+
+if (formReserva) {
+    formReserva.addEventListener("submit", function(event) {
+        // Evita que a página recarregue ao enviar o formulário
+        event.preventDefault();
+
+        // Captura os valores digitados pelo usuário
+        const mesaEscolhida = document.getElementById("mesa").value;
+        const qtdPessoas = document.getElementById("pessoas").value;
+
+        // Cria um objeto JavaScript com os dados
+        const dadosDaReserva = {
+            mesa: mesaEscolhida,
+            pessoas: qtdPessoas,
+            dataRegistro: new Date().toLocaleString()
+        };
+
+        // Converte o objeto em texto (JSON) e salva no Local Storage
+        localStorage.setItem("minhaReservaBravos", JSON.stringify(dadosDaReserva));
+
+        alert(`Sucesso! Reserva confirmada para ${qtdPessoas} pessoas na mesa: ${mesaEscolhida}.`);
+        
+        // Redireciona de volta para a Home após o sucesso
+        window.location.href = "index.html";
     });
 }
